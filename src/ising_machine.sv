fd5db03ef1e12e70d40ecf12e8280a18689b5a2b
andychiem19 <andy.chiem19@gmail.com>
2025-12-05 02:16:59 -0500

    module framework changes

--- src/ising_machine.sv
+++ src/ising_machine.sv
@@ -5,18 +5,18 @@ module ising_machine #(
 )(
     input   logic clk,
     input   logic n_rst,
-    input   logic start,
-    // a 2D coupling matrix, couplingMatrix[i][j] describes relationships between oscillators i and j
-    input   logic signed [dataWidth-1:0] couplingMatrix [N-1:0][N-1:0], 
-    // final states of N oscillators, now in a 1D array
-    input   logic signed [dataWidth-1:0] stopTime,
+    input   logic start,  // a 2D coupling matrix, couplingMatrix[i][j] describes relationships between oscillators i and j
+    input   logic signed [dataWidth-1:0] couplingMatrix [N-1:0][N-1:0],  // final states of N oscillators, now in a 1D array
+    input   logic signed [dataWidth-1:0] stopTime,  
     input   logic signed [dataWidth-1:0] deltaT,
     output  logic signed [dataWidth-1:0] finalPhases [N-1:0]
     output  logic done
 );
 
-typedef enum logic [1:0] {
+typedef enum logic [2:0] {
     IDLE,
+    INIT,
     COMPUTE,
-    DONE
+    UPDATE,
+    FINISH
 };
